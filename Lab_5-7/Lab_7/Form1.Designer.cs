7917134390bb91ce8d7f35ca630f378ff95f66b6
4lnULpGkuW <yapbidido@yandex.ru>
2025-03-27 15:25:38 +0300

    Lab_7

---
+++ Lab_5-7/Lab_7/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Lab_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.numericUpDownCapacity = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownDemand = new System.Windows.Forms.NumericUpDown();
+            this.trackBarThreshold = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelArrivals = new System.Windows.Forms.Label();
+            this.labelShipments = new System.Windows.Forms.Label();
+            this.labelSupply = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCapacity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDemand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarThreshold)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(11, 109);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Запасы";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Отгрузки";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Поступления";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(777, 329);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // numericUpDownCapacity
+            // 
+            this.numericUpDownCapacity.Location = new System.Drawing.Point(191, 16);
+            this.numericUpDownCapacity.Name = "numericUpDownCapacity";
+            this.numericUpDownCapacity.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownCapacity.TabIndex = 1;
+            this.numericUpDownCapacity.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownDemand
+            // 
+            this.numericUpDownDemand.Location = new System.Drawing.Point(414, 16);
+            this.numericUpDownDemand.Name = "numericUpDownDemand";
+            this.numericUpDownDemand.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownDemand.TabIndex = 2;
+            this.numericUpDownDemand.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // trackBarThreshold
+            // 
+            this.trackBarThreshold.LargeChange = 1;
+            this.trackBarThreshold.Location = new System.Drawing.Point(191, 51);
+            this.trackBarThreshold.Name = "trackBarThreshold";
+            this.trackBarThreshold.Size = new System.Drawing.Size(120, 56);
+            this.trackBarThreshold.TabIndex = 3;
+            this.trackBarThreshold.Value = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Вместимость склада";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Минимальный уровень \r\nзапасов";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(352, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Спрос";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(566, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Запасы на складе";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(566, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Отгрузки";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(566, 76);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Поступления";
+            // 
+            // labelArrivals
+            // 
+            this.labelArrivals.AutoSize = true;
+            this.labelArrivals.Location = new System.Drawing.Point(705, 76);
+            this.labelArrivals.Name = "labelArrivals";
+            this.labelArrivals.Size = new System.Drawing.Size(14, 16);
+            this.labelArrivals.TabIndex = 12;
+            this.labelArrivals.Text = "0";
+            // 
+            // labelShipments
+            // 
+            this.labelShipments.AutoSize = true;
+            this.labelShipments.Location = new System.Drawing.Point(705, 47);
+            this.labelShipments.Name = "labelShipments";
+            this.labelShipments.Size = new System.Drawing.Size(14, 16);
+            this.labelShipments.TabIndex = 11;
+            this.labelShipments.Text = "0";
+            // 
+            // labelSupply
+            // 
+            this.labelSupply.AutoSize = true;
+            this.labelSupply.Location = new System.Drawing.Point(705, 18);
+            this.labelSupply.Name = "labelSupply";
+            this.labelSupply.Size = new System.Drawing.Size(14, 16);
+            this.labelSupply.TabIndex = 10;
+            this.labelSupply.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 32);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Старт/стоп";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelArrivals);
+            this.Controls.Add(this.labelShipments);
+            this.Controls.Add(this.labelSupply);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBarThreshold);
+            this.Controls.Add(this.numericUpDownDemand);
+            this.Controls.Add(this.numericUpDownCapacity);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Lab_7";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCapacity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDemand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarThreshold)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown numericUpDownCapacity;
+        private System.Windows.Forms.NumericUpDown numericUpDownDemand;
+        private System.Windows.Forms.TrackBar trackBarThreshold;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelArrivals;
+        private System.Windows.Forms.Label labelShipments;
+        private System.Windows.Forms.Label labelSupply;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab_5-7/Lab_7/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int capacity, threshold, demand, supply, day, arrivals, shortage;
+        Random random = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                return;
+            }
+            day = 0;
+            capacity = (int)numericUpDownCapacity.Value;
+            threshold = (int)((double)capacity / 10.0 * (double)trackBarThreshold.Value);
+            demand = (int)numericUpDownDemand.Value;
+            supply = random.Next(0, capacity);
+            foreach (var i in chart1.Series ) { i.Points.Clear(); }
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            day++;
+
+            chart1.Series[2].Points.AddXY(day, arrivals);
+            labelArrivals.Text = arrivals.ToString();
+
+            int dailyDemand = random.Next(demand / 2, demand * 2);
+            int shipments;
+            if (supply + arrivals >= shortage + dailyDemand)
+                shipments = shortage + dailyDemand;
+            else
+                shipments = supply + arrivals;
+            supply += arrivals - (shortage + dailyDemand);
+            if (supply < 0)
+            {
+                shortage = -supply;
+                supply = 0;
+            }
+            else shortage = 0;
+            if (supply < threshold || shortage > 0)
+                arrivals = random.Next(threshold, capacity + shortage);
+            else arrivals = 0;
+
+            chart1.Series[0].Points.AddXY(day, supply);
+            chart1.Series[1].Points.AddXY(day, shipments);
+
+            labelSupply.Text = supply.ToString();
+            labelShipments.Text = shipments.ToString();
+        }
+    }
+}
